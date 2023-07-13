654547232465d0bb4dfe8a5fe28c60b3bb982a34
Sachin_Cherey <64123014+sachinCherey@users.noreply.github.com>
2023-07-13 14:02:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -16,13 +16,13 @@ class Person {
 
 class Student extends Person {
 	study(){
-		console.log(getter()+"is studying");
+		console.log(this.name +"is studying");
 	}
 }
 
 class Teacher extends Person {
 	teach(){
-		console.log(getter()+"is teaching")
+		console.log(this.name +"is teaching")
 	}
 }
 
